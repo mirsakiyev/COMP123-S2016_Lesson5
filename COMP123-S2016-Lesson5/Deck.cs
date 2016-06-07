b9051858ef484c8a85ca2c63ba978d858b51b9f9
mirsakiyev <mirsakiyev@yahoo.com>
2016-06-07 14:04:02 -0400

    Created Deck class

---
+++ COMP123-S2016-Lesson5/Deck.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP123_S2016_Lesson5
+{
+    /**
+    * <summary>
+    * This class extends the List<Card> generic type
+    * </summary>
+    * 
+    * @class Deck
+    */
+    public class Deck : List<Card>
+    {
+        // CONSTRUCTOR ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+        * <summary>
+        * This is the default constructor for the Deck class
+        * </summary>
+        * 
+        * @construtor Deck
+        */
+        public Deck()
+        {
+            this._create();
+        }
+
+        // PRIVATE METHODS+++++++++++++++++++++++++++++++++++
+        /**
+        * <summary>
+        * This method creates a deck of cards
+        * </summary>
+        * 
+        * @method _create
+        * @return {void}
+        */
+        private void _create()
+        {
+            string suit = "";
+
+            for (int i = 0; i < 4; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        suit = "hearts";
+                        break;
+                    case 1:
+                        suit = "clubs";
+                        break;
+                    case 2:
+                        suit = "diamonds";
+                        break;
+                    case 3:
+                        suit = "spades";
+                        break;
+
+                }
+                for (int face = 1; face < 14; face++)
+                {
+                    this.Add(new Card(face, suit));
+                } //end for -face
+            } // end for -suit
+        } // end CreateDeck method
+
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+
+        /**
+        * <summary>
+        * This method displays a List of Card objects to the Console
+        * </summary>
+        * 
+        * @method Display
+        * @return {void}
+        */
+        public void Display()
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine("+ Current Deck                              +");
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
+
+            foreach (Card card in this)
+            {
+                Console.WriteLine("{0} of {1}", card.Face, card.Suit);
+            }
+            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
+            Console.WriteLine();
+        }
+
+
+        /**
+       * <summary>
+       * This method randomly shuffles a List of Card objects
+       * </summary>
+       * 
+       * @method Shuffle
+       * @return {void}
+       */
+        public void Shuffle()
+        {
+            // creates a psedo-random number sequence and stores it is random
+            Random random = new Random();
+            // record the number of cards in the deck list
+            int cardCount = this.Count;
+
+            // iterate through the list of cards
+            for (int currentCard = 0; currentCard < cardCount; currentCard++)
+            {
+                Card tempCard = this[currentCard]; // copy current card to temp location
+                int randomCard = random.Next(0, cardCount); // get a random card index
+                this[currentCard] = this[randomCard]; // copy value from randomCard to current card
+                this[randomCard] = tempCard; // copy current card to random card
+            }
+        }
+    }  
+}
--- COMP123-S2016-Lesson5/Program.cs
+++ COMP123-S2016-Lesson5/Program.cs
@@ -21,107 +21,13 @@ namespace COMP123_S2016_Lesson5
          */
         public static void Main(string[] args)
         {
-            //create instance of the Student class
+            Deck deck = new Deck();
 
-            List<Card> Deck = new List<Card>();
-
-            CreateDeck(Deck);
-            DisplayDeck(Deck); // display initial state
-
-            ShuffleDeck(Deck);
-            DisplayDeck(Deck);
+            deck.Display();
+            deck.Shuffle();
+            deck.Display();
         }
-
-
-        /**
-        * <summary>
-        * This method creates a deck of cards
-        * </summary>
-        * 
-        * @method CreateDeck
-        * @param {List<Card>} deck
-        * @return {void}
-        */
-        public static void CreateDeck(List<Card> deck)
-        {
-            string suit = "";
-
-            for (int i = 0; i < 4; i++)
-            {
-                switch (i)
-                {
-                    case 0:
-                        suit = "hearts";
-                        break;
-                    case 1:
-                        suit = "clubs";
-                        break;
-                    case 2:
-                        suit = "diamonds";
-                        break;
-                    case 3:
-                        suit = "spades";
-                        break;
-
-                }
-                for (int face = 1; face < 14; face++)
-                {
-                    deck.Add(new Card(face, suit));
-                } //end for -face
-            } // end for -suit
-        } // end CreateDeck method
-
-
-        /**
-        * <summary>
-        * This method displays a List of Card objects to the Console
-        * </summary>
-        * 
-        * @method DisplayDeck
-        * @param {List<Card>} deck
-        * @return {void}
-        */
-        public static void DisplayDeck(List<Card> deck)
-        {
-            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
-            Console.WriteLine("+ Current Deck                              +");
-            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
-
-            foreach (Card card in deck)
-            {
-                Console.WriteLine("{0} of {1}", card.Face, card.Suit);
-            }
-            Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++++");
-            Console.WriteLine();
-        }
-
-
-        /**
-       * <summary>
-       * This method randomly shuffles a List of Card objects
-       * </summary>
-       * 
-       * @method ShuffleDeck
-       * @param {List<Card>} deck
-       * @return {void}
-       */
-        public static void ShuffleDeck(List<Card> deck)
-        {
-            // creates a psedo-random number sequence and stores it is random
-            Random random = new Random();
-            // record the number of cards in the deck list
-            int cardCount = deck.Count;
-
-            // iterate through the list of cards
-            for (int currentCard = 0; currentCard < cardCount; currentCard++)
-            {
-                Card tempCard = deck[currentCard]; // copy current card to temp location
-                int randomCard = random.Next(0,cardCount); // get a random card index
-                deck[currentCard] = deck[randomCard]; // copy value from randomCard to current card
-                deck[randomCard] = tempCard; // copy current card to random card
-            }
-        }
-
+            
     } // end Program
 
 } // end namespace
